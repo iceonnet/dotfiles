a6da250f1fd710a1ef9ef0882b3e56183c75197a
Jon Erling Hustadnes <jonerling.hustadnes@inmarsat.com>
2017-08-30 16:50:37 +0200

    tweaked compton startup commented out default audio sink

--- bin/startup.sh
+++ bin/startup.sh
@@ -16,10 +16,10 @@ xset s off off -dpms
     (~/bin/nvidiafanspeed/nvidiafanspeed.sh &) && notify-send -u critical "[nvidiafanspeed] started"
 
 # Compton
-    (compton -D 5 -f --glx-copy-from-front &) && notify-send "[compton] started"
+    (compton -D 1 -f --glx-copy-from-front --inactive-dim 0.05 &) && notify-send "[compton] started"
 
 # Setup Default audio sink
-    pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo &
+    # pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo &
 
 # Setup monitors
     sleep 5
